5b581c996dae9cefa2e62bb81e2f3074b8419b4b
Alejandro Lavado <alejandro.lvd@gmail.com>
2020-09-28 13:38:13 -0300

    lee datos desde archivo

--- src/components/LecturaDatos/LecturaDatos.js
+++ src/components/LecturaDatos/LecturaDatos.js
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './LecturaDatos.css'
 
 const LecturaDatos = () => {
+
+  const archivo = useRef()
+
+  const leerArchivo = () => {
+    archivo.current.files[0].text()
+      .then(texto => {
+        const lineas = texto.split('\n')
+        const datos = []
+        lineas.slice(1).forEach(linea => {
+          const [id,,,, peso, largo] = linea.split(',')
+          datos.push([id, peso, largo])
+        })
+      })
+  }
+
   return (
     <div className="LecturaDatos">
-      
+      <input ref={archivo} type="file" accept=".csv" />
+      <button onClick={leerArchivo}>Leer</button>
     </div>
   )
 }
