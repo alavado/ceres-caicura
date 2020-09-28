0017b5e7d82b477600560bc2dcdd63baf7fb8998
Alejandro Lavado <alejandro.lvd@gmail.com>
2020-09-28 12:16:53 -0300

    cra

---
+++ src/components/Login/Login.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+      <h1>CERES BCA</h1>
+      <h2>Acceso a plataforma de modalemiento hundimiento Caicura</h2>
+      <form>
+        <label>Ingrese su contraseña
+          <input type="password" />
+        </label>
+        <button type="submit">Ingresar</button>
+      </form>
+    </div>
+  )
+}
+
+export default Login
